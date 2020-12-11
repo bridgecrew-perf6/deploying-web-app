37c6f7906ecae0c28113dd4a386a08af3f1f61b4
Bill Xiong <xiong.bill@yahoo.com>
2020-12-10 22:04:36 -0800

    added fitness URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <main>
       <h1>My Portfolio</h1>
+      <div>
+        <ul>
+          <li><a href="https://github.com/anywhere-fitness-1/front-end/tree/bill-xiong"></a></li>
+        </ul>
+      </div>
       
     </main>
   );
